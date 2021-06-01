8293a33aaf4735e01120f49d6a112b6782454068
Won Yang <won@gatech.edu>
2021-05-31 19:27:00 -0500

    adds js solution

--- js/stockPicker.js
+++ js/stockPicker.js
@@ -1,3 +1,29 @@
-exports.picker = function(prices) {
+const picker = function (prices) {
+  // Temp values to mark current position in array
+  let currentBuyDate = 0;
+  let currentSellDate = 1;
 
+  // Values to return
+  let buyDate = 0;
+  let sellDate = 0;
+
+  let maxProfit = 0;
+
+  while (currentBuyDate != prices.length - 1) {  // loop thru prices until currentBuyDate is at last element
+    for (currentSellDate = currentBuyDate + 1; currentSellDate < prices.length; currentSellDate++) {  // currentSellDate starts at currentBuyDate + 1 and loops to the end of prices
+      if (prices[currentSellDate] - prices[currentBuyDate] > maxProfit) {  // determining maxProfit
+        maxProfit = prices[currentSellDate] - prices[currentBuyDate]
+        buyDate = currentBuyDate;
+        sellDate = currentSellDate;
+      }
+    }
+
+    currentBuyDate++;
+    currentSellDate++;
+  }
+
+  return [buyDate, sellDate];
 }
+
+
+module.exports = picker;
